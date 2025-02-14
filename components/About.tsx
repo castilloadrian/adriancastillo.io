597625f21539dcbe90759d20e3e2c68e55288e83
Adrian Castillo <adriancastillo1222@gmail.com>
2025-02-14 12:57:50 -0500

    adding hover styling and link to hellofresh

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,21 @@
+import Link from 'next/link'
+
 export default function About() {
   return (
     <section className="space-y-8">
     <h1 className="text-4xl font-medium">adriancastillo.io</h1>
     <p className="text-lg leading-relaxed">
       hi! i'm adrian, a software engineer based in new york city. currently building 
-      personal projects in robotics and ai while scaling distributed systems @hellofresh. passionate about 
-      ai/ml and reimagining technical possibilities.
+      personal projects in robotics and ai while scaling distributed systems @
+      <Link 
+        href="https://www.hellofresh.com"
+        className="hover:underline underline-offset-4"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        hellofresh
+      </Link>
+      . passionate about ai/ml and reimagining technical possibilities.
     </p>
     <p className="text-lg">
       this year, i want to build more things.
@@ -13,4 +23,3 @@ export default function About() {
   </section>
   )
 }
-
